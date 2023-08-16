e998573b03281ca5e2b6b32b5f3b8a389fab907a
EcaCosca <enriquecoscarelli@gmail.com>
2023-08-16 15:31:50 +0200

    Cleaning up code

--- app.js
+++ app.js
@@ -5,21 +5,9 @@ const path = require('path')
 
 const port = process.env.PORT || 3000
 
-// app.get('/', (req, res)=>{
-//     res.send("Welcome!")
-// })
-
-// app.put('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
-
 app.route('/')
-.get((req, res)=>{
-    res.send("Welcome!")
-})
-.put((req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+.get((req, res)=>{res.send("Welcome!")})
+.put((req, res) => {res.sendFile(path.join(__dirname, 'index.html'));});
 
 app.listen(port,()=>{
   console.log(`http://localhost:${port}/`);
