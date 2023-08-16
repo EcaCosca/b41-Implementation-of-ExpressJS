67b1e9ec07e95eb4593e192055e64686ac94a15b
EcaCosca <enriquecoscarelli@gmail.com>
2023-08-16 15:37:27 +0200

    done

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 3000
 app.route('/')
 .get((req, res)=>{res.send("Welcome!")})
 .put((req, res) => {res.sendFile(path.join(__dirname, 'index.html'));})
+.delete((req, res) => {res.status(202).json({good:"yep"});})
 
 app.listen(port,()=>{
   console.log(`http://localhost:${port}/`);
