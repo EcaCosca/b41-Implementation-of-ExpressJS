b5296f3673f10a7926a6002e6eb9eae9a4e159b8
EcaCosca <enriquecoscarelli@gmail.com>
2023-08-16 15:54:12 +0200

    Done with ex4

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ require('dotenv').config()
 const express = require('express');
 const app = express();
 const path = require('path')
+let ejs = require('ejs');
+
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
 
 const port = process.env.PORT || 3000
 
@@ -9,6 +13,10 @@ app.route('/')
 .get((req, res)=>{res.send("Welcome!")})
 .put((req, res) => {res.sendFile(path.join(__dirname, 'index.html'));})
 .delete((req, res) => {res.status(202).json({good:"yep"});})
+
+app.get('/test-ejs', (req, res) => {
+    res.render('./pages/index.ejs', {myTitle:"my first title"});
+})
 
 app.listen(port,()=>{
   console.log(`http://localhost:${port}/`);
